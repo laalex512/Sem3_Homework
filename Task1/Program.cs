f5bcf4e1580d9dedce7d9d26b54bbc693cb70336
laalex <laalex512@gmail.com>
2022-06-16 00:01:46 +0300

    add Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Insert number");
+int num = Convert.ToInt32(Console.ReadLine());
+int numReverse = 0;
+int copy = num;
+
+
+while (copy > 0)
+{
+    numReverse = numReverse * 10 + copy % 10;
+    copy /= 10;
+}
+if (num==numReverse)
+{
+    Console.WriteLine("Yes, It's palindrom ");
+}
+else    
+{
+    Console.WriteLine("No, It isn't palindrom ");
+}
+
