93d0b8be0e0d8680d8adcbd10f7680b373d37967
laalex <laalex512@gmail.com>
2022-06-16 19:32:06 +0300

    Add Additional task3

---
+++ AdditionalTask3/Program.cs
@@ -0,0 +1,32 @@
+﻿/*3. Игра угадайка. Программа загадывает случайное число. Пользователь его угадывает. 
+Если пользователь дает неправильный ответ, то программа сообщает, больше загаданное число или меньше.
+На отгадывание дается 3 попытки.*/
+
+string Input(string mes)
+{
+    Console.Write($"Insert {mes}: ");
+    return Console.ReadLine();
+}
+
+int number = new Random().Next(1,10);
+
+//Console.WriteLine(number);
+
+for (int i = 0; i < 3; i++)
+{
+    int attemptNum = Convert.ToInt32(Input($"number ({3-i} tries left)"));
+    if (attemptNum==number)
+    {
+        Console.WriteLine("You win!");
+        break;
+    }
+    else if (attemptNum>number)
+    {
+        Console.WriteLine("No. <");
+    }
+    else
+    {
+        Console.WriteLine("No. >");
+    }
+}
+
